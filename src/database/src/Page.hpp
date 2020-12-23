b7d73ec8bf8f2a8a80d14570c4530e7d0b705eb8
Maciej Jablonski <maciej.jablonski@intel.com>
2020-12-23 12:53:40 +0100

    Changed clean and dirty pages to templates using page_memory and custom entries

--- src/database/src/Page.hpp
+++ src/database/src/Page.hpp
@@ -2,84 +2,111 @@
 #define DATABASE_AREA_ENTRY_PAGE_HPP
 
 #include <vector>
-#include "Entry.hpp"
+#include <stdexcept>
+
 namespace page
 {
-
-using PODPage = std::vector<std::byte>;
-
-struct PODEntryPageHeader
+template <typename PageMemory>
+concept memory_access = requires
 {
-    int32_t records_number;
+    PageMemory::data(), PageMemory::size(), PageMemory::begin(), PageMemory::end();
 };
-
-template <typename Derived>
-class Page
+template <typename PageMemory, typename Entry>
+requires memory_access<PageMemory>&& std::is_trivial_v<Entry> class PageWithEntries
 {
    public:
-    using clean_entry =
-        std::variant<const entries::CleanEntry, const entries::DeletedEntry>;
-    Page(PODPage&& page);
-    Page(size_t page_size);
-    inline size_t Capacity() const
-    {
-        return (memory_.size() - sizeof(decltype(*header_))) /
-               sizeof(entries::PODEntry);
-    }
+    PageWithEntries(PageMemory&& page);
+    inline size_t Capacity() const { return (memory_.size() - sizeof(*header_)) / sizeof(Entry); }
     inline size_t Size() const { return header_ ? header_->records_number : 0; }
-    const clean_entry& View(size_t idx) const;
-    PODPage&& Release();
+    const Entry& View(size_t idx) const;
+    PageMemory&& Release();
 
    protected:
-    inline size_t OffsetTo(size_t idx) const
+    size_t OffsetTo(size_t idx) const;
+    PageMemory memory_;
+    struct
     {
-        return idx * sizeof(entries::PODEntry) + sizeof(PODEntryPageHeader);
-    }
-    PODPage memory_;
-    PODEntryPageHeader* header_;
+        size_t entries;
+    } * header_;
 };
 
-class CleanPage : public Page<CleanPage>
+template <typename PageMemory, typename Entry>
+class CleanPageWithEntries : public PageWithEntries<PageMemory, Entry>
 {
 };
 
-class DirtyPage : public Page<DirtyPage>
+template <typename PageMemory, typename Entry>
+class DirtyPageWithEntries : public Page<PageMemory, Entry>
 {
-
    public:
-    DirtyPage(CleanPage&& clean_page);
-    DirtyPage& operator=(CleanPage&& clean_page);
-    void Write(const entries::PODEntry& entry, size_t idx);
-    void Append(const entries::PODEntry& entry);
+    DirtyPageWithEntries(CleanPageWithEntries<PageMemory, Entry>&& clean_page);
+    DirtyPageWithEntries& operator=(CleanPageWithEntries<PageMemory, Entry>&& clean_page);
+    void Write(const Entry& entry, size_t idx);
+    void Append(const Entry& entry);
 };
-
-template <typename Derived>
-inline Page<Derived>::Page(PODPage&& page)
-    : memory_(std::move(page)),
-      header_(reinterpret_cast<PODEntryPageHeader*>(memory_.data()))
+template <typename PageMemory, typename Entry>
+inline PageWithEntries<PageMemory, Entry>::PageWithEntries(PageMemory&& page)
+    : memory_(std::move(page)), header_(reinterpret_cast<Header*>(memory_.data()))
 {
 }
-template <typename Derived>
-inline Page<Derived>::Page(size_t page_size)
-    : memory_(page_size),
-      header_(reinterpret_cast<PODEntryPageHeader*>(memory_.data()))
-{
-}
-template <typename Derived>
-inline const Page<Derived>::clean_entry& Page<Derived>::View(size_t idx) const
-{
-    if (idx >= Count())
-    {
-        throw std::out_of_range("Attempted to view entry outside range.");
-    }
-    auto offset = sizeof(*header_) + Count() * idx;
-    return *reinterpret_cast<const Entry*>(memory_.data() + offset);
-}
-template <typename Derived>
-inline PODPage&& Page<Derived>::Release()
+template <typename PageMemory, typename Entry>
+inline PageMemory&& PageWithEntries<PageMemory, Entry>::Release()
 {
     header_ = nullptr;
     return std::move(memory_);
+}
+template <typename PageMemory, typename Entry>
+inline size_t PageWithEntries<PageMemory, Entry>::OffsetTo(size_t idx) const
+{
+    return sizeof(Header) + idx * sizeof(Entry);
+}
+
+template <typename PageMemory, typename Entry>
+inline const Entry& PageWithEntries<PageMemory, Entry>::View(size_t idx) const 
+{
+    if (idx >= Size())
+    {
+        throw std::out_of_range("Attempted to read behind page index.");
+    }
+    return *reinterpret_cast<const Entry&>(memory_.data() + OffsetTo(idx));
+}
+
+template <typename PageMemory, typename Entry>
+inline DirtyPageWithEntries<PageMemory, Entry>::DirtyPageWithEntries(
+    CleanPageWithEntries<PageMemory, Entry>&& clean_page)
+    : PageWithEntries<PageMemory, Entry>(clean_page.Release())
+{
+}
+
+template <typename PageMemory, typename Entry>
+inline DirtyPageWithEntries<PageMemory, Entry>& DirtyPageWithEntries<PageMemory, Entry>::operator=(
+    CleanPageWithEntries<PageMemory, Entry>&& clean_page)
+{
+    memory_ = clean_page.Release();
+    header_ = reinterpret_cast<Header*>(memory_.data());
+}
+
+template <typename PageMemory, typename Entry>
+void page::DirtyPageWithEntries<PageMemory, Entry>::Write(const Entry& entry, size_t idx)
+{
+    if (idx > Size())
+    {
+        throw std::out_of_range("Attempted to write behind page index.");
+    }
+    if (idx == Capacity())
+    {
+        throw std::out_of_range("Page is full.");
+    }
+    ++header_->entries;
+    std::copy_n(
+        &entry, 1,
+        reinterpret_cast<Entry*>(memory_.data() + OffsetTo(idx)));
+}
+
+template <typename PageMemory, typename Entry>
+void page::DirtyPageWithEntries<PageMemory, Entry>::Append(const Entry & entry)
+{
+    Write(entry, Size());
 }
 }  // namespace page
 
