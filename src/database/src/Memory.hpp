50245add6c5eba7628b66c053d66604ba8c5b4f5
Maciej Jablonski <maciej.jablonski@intel.com>
2020-12-24 15:50:06 +0100

    Added page memory

---
+++ src/database/src/Memory.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+namespace page
+{
+
+constexpr auto DEFAULT_PAGE_MEMORY = 4096U;
+
+template <size_t size>
+using continuous_memory = std::array<std::byte, size>;
+
+template <size_t page_size = DEFAULT_PAGE_MEMORY>
+class PageMemory : public continuous_memory<page_size>
+{
+   public:
+    static constexpr size_t size = page_size;
+    PageMemory(size_t index);
+    PageMemory(size_t index, continuous_memory<page_size>&& memory);
+    inline auto Index() const { return index_; }
+
+   private:
+    size_t index_;
+};
+
+template <size_t page_size> 
+inline PageMemory<page_size>::PageMemory(size_t index) : continuous_memory<page_size>(page_size), index_(index)
+{
+}
+template <size_t page_size>
+inline PageMemory<page_size>::PageMemory(size_t index, continuous_memory<page_size>&& memory)
+    : continuous_memory<page_size>(std::move(memory)), index_(index)
+{
+}
+}  // namespace page
