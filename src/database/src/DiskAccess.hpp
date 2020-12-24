f81e59855fa7f7fa37aa813bbfd32876a4cfe63c
Maciej Jablonski <maciej.jablonski@intel.com>
2020-12-24 15:55:28 +0100

    Added disk access counter class

---
+++ src/database/src/DiskAccess.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+struct DiskAccess
+{
+    int reads;
+    int writes;
+    inline void Write() { ++writes; }
+    inline void Read() { ++reads; }
+    inline int CountWrites() const { return writes; }
+    inline int CountReads() const { return reads; }
+    inline void Reset() { writes = reads = 0; }
+};
